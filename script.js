3193759918caae6b76dc1148919daeccf9f1a036
DmitriyBartenev <bartenevdmitriy@bk.ru>
2022-05-13 15:49:13 +0300

    test

--- script.js
+++ script.js
@@ -26,4 +26,9 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = "hidden";
     });
     
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+      });
 });
